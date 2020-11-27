132e814b266a27623c1fa2ae74dc587c1854322d
Juan Ro <juanro.pf@gmail.com>
2020-11-27 00:11:30 -0600

    docs added

---
+++ docs/precache-manifest.7a27a749eec831640e2d7c72723005de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0fc186bb331a5e3609517e0adf6628d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca5add5e92de229bd63e",
+    "url": "/static/css/main.e278dd49.chunk.css"
+  },
+  {
+    "revision": "a385849cea1c1ea54578",
+    "url": "/static/js/2.e25b4697.chunk.js"
+  },
+  {
+    "revision": "5964d60f643ed1ea9d3113caf4506574",
+    "url": "/static/js/2.e25b4697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5add5e92de229bd63e",
+    "url": "/static/js/main.4dc37882.chunk.js"
+  },
+  {
+    "revision": "49de1d56170d62cf3751",
+    "url": "/static/js/runtime-main.d0798475.js"
+  }
+]);
